774f3c4ec8934955b8e6c5a3584b01adc26aff02
Zafer Celenk (thinkpad) <zafercelenk@gmail.com>
2023-04-12 21:43:42 +0000

    Temel sistem tasarimi yapildi.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,117 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mt-4">
+    <div class="row my-4">
+        <div class="col-10">
+            <h1>Benim Panom</h1>
+            <p>
+                Dokümanlarınızı hızlı bir sekilde kayıt altına alabilir ve daha
+                sonra ihtiyaç duyduğunuzda hızlıca ulaşabilirsiniz. Panom size
+                kolay, pratik ve basit bir çözüm sunar. <a
+                    href="https://panom.me/help"
+                    >daha fazla bilgi için tıklayın.</a
+                >
+            </p>
+        </div>
+    </div>
+
+    <div class="row g-3">
+        <div class="col-xs-12 col-2">
+            <a href="/" class="boxLink">
+                <div class="contentBox">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Unde maxime consequuntur autem suscipit consectetur
+                    exercitationem accusantium. Debitis quae laboriosam illum et
+                    quis nisi alias molestias! Soluta voluptate excepturi harum
+                    commodi magni alias sequi quos obcaecati, et dolore
+                    sapiente? Eligendi recusandae fuga quas deleniti, at eaque
+                    non.
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-12 col-2">
+            <a href="/" class="boxLink">
+                <div class="contentBox">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Unde maxime consequuntur autem suscipit consectetur
+                    exercitationem accusantium. Debitis quae laboriosam illum et
+                    quis nisi alias molestias! Soluta voluptate excepturi harum
+                    commodi magni alias sequi quos obcaecati, et dolore
+                    sapiente? Eligendi recusandae fuga quas deleniti, at eaque
+                    non.
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-12 col-2">
+            <a href="/" class="boxLink">
+                <div class="contentBox">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    
+                </div>
+            </a>
+        </div>
+        <div class="col-2">
+            <a href="/" class="boxLink">
+                <div class="contentBox">
+                    &lt;!DOCTYPE html&gt;
+&lt;html lang=&quot;en&quot;&gt;
+	&lt;head&gt;
+		&lt;meta charset=&quot;utf-8&quot; /&gt;
+		&lt;link rel=&quot;icon&quot; href=&quot;%sveltekit.assets%/favicon.png&quot; /&gt;
+		&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width&quot; /&gt;
+    	
+                </div>
+            </a>
+        </div>
+        <div class="col-2">
+            <a href="/" class="boxLink">
+                <div class="contentBox">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Unde maxime consequuntur autem suscipit consectetur
+                    exercitationem accusantium. Debitis quae laboriosam illum et
+                    quis nisi alias molestias! Soluta voluptate excepturi harum
+                    commodi magni alias sequi quos obcaecati, et dolore
+                    sapiente? Eligendi recusandae fuga quas deleniti, at eaque
+                    non.
+                </div>
+            </a>
+        </div>
+        <div class="col-2">
+            <a href="/" class="boxLink">
+                <div class="contentBox">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Unde maxime consequuntur autem suscipit consectetur
+                    exercitationem accusantium. Debitis quae laboriosam illum et
+                    quis nisi alias molestias! Soluta voluptate excepturi harum
+                    commodi magni alias sequi quos obcaecati, et dolore
+                    sapiente? Eligendi recusandae fuga quas deleniti, at eaque
+                    non.
+                </div>
+            </a>
+        </div>
+        <div class="col-2">
+            <a href="/" class="boxLink">
+                <div class="contentBox">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Unde maxime consequuntur autem suscipit consectetur
+                    exercitationem accusantium. Debitis quae laboriosam illum et
+                    quis nisi alias molestias! Soluta voluptate excepturi harum
+                    commodi magni alias sequi quos obcaecati, et dolore
+                    sapiente? Eligendi recusandae fuga quas deleniti, at eaque
+                    non.
+                </div>
+            </a>
+        </div>
+        <div class="col-2">
+            <a href="/" class="boxLink">
+                <div class="contentBox">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Unde maxime consequuntur autem suscipit consectetur
+                    exercitationem accusantium. Debitis quae laboriosam illum et
+                    quis nisi alias molestias! Soluta voluptate excepturi harum
+                    commodi magni alias sequi quos obcaecati, et dolore
+                    sapiente? Eligendi recusandae fuga quas deleniti, at eaque
+                    non.
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
